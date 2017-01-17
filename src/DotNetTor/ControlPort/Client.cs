ae8af0fce36046ae13722cb3e3f7952614b964b3
nopara73 <adam.ficsor73@gmail.com>
2017-01-17 17:32:02 +0900

    Multiple circuit changes fixed.

--- src/DotNetTor/ControlPort/Client.cs
+++ src/DotNetTor/ControlPort/Client.cs
@@ -16,29 +16,7 @@ namespace DotNetTor.ControlPort
 			_controlEndPoint = new IPEndPoint(IPAddress.Parse(address), controlPort);
 			_password = password;
 		}
-
-		private async Task AssertControlPortPasswordAsync()
-		{
-			using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-			{
-				await socket.ConnectAsync(_controlEndPoint).ConfigureAwait(false);
-				var command = new ArraySegment<byte>(Encoding.ASCII.GetBytes($"authenticate \"{_password}\"\r\n"));
-
-				await socket.SendAsync(command, SocketFlags.None).ConfigureAwait(false);
-
-				var buffer = new ArraySegment<byte>(new byte[128]);
-				var received = await socket.ReceiveAsync(buffer, SocketFlags.None).ConfigureAwait(false);
-
-				if (received != 0)
-				{
-					var response = Encoding.ASCII.GetString(buffer.Array, 0, received);
-
-					if (!response.StartsWith("250", StringComparison.Ordinal))
-						throw new TorException("Possibly wrong TOR control port password");
-				}
-			}
-		}
-
+		
 		[Obsolete(Shared.SyncMethodDeprecated)]
 		public void ChangeCircuit()
 		{
@@ -79,6 +57,18 @@ namespace DotNetTor.ControlPort
 					if (statusCode != StatusCode.OK)
 						throw new TorException($"Possibly wrong TOR control port password. {nameof(statusCode)}: {statusCode}");
 
+					// 3. SET LISTEN FOR CIRCUIT STATUS CHANGED EVENTS
+					command = new ArraySegment<byte>(Encoding.ASCII.GetBytes("SETEVENTS CIRC\r\n"));
+					await socket.SendAsync(command, SocketFlags.None).ConfigureAwait(false);
+
+					buffer = new ArraySegment<byte>(new byte[128]);
+					received = await socket.ReceiveAsync(buffer, SocketFlags.None).ConfigureAwait(false);
+
+					response = Encoding.ASCII.GetString(buffer.Array, 0, received);
+					statusCode = GetStatusCode(response);
+					if (statusCode != StatusCode.OK)
+						throw new TorException($"SETEVENTS CIRC failed {nameof(statusCode)}: {statusCode}");
+
 					// 3. CHANGE CIRCUIT
 					command = new ArraySegment<byte>(Encoding.ASCII.GetBytes("signal newnym\r\n"));
 					await socket.SendAsync(command, SocketFlags.None).ConfigureAwait(false);
@@ -91,6 +81,30 @@ namespace DotNetTor.ControlPort
 					if (statusCode != StatusCode.OK)
 						throw new TorException($"Couldn't change the circuit. {nameof(statusCode)}: {statusCode}");
 
+					// LOOK FOR CIRCUIT CLOSING EVENTS
+					var timeout = Task.Delay(TimeSpan.FromMinutes(3));
+					string[] responseArray;
+					do
+					{
+						var t1 = socket.ReceiveAsync(buffer, SocketFlags.None);
+
+						await Task.WhenAny(t1, timeout).ConfigureAwait(false);
+						if (t1.IsCompleted)
+							received = t1.Result;
+						else throw new TimeoutException();
+
+						response = Encoding.ASCII.GetString(buffer.Array, 0, received);
+						responseArray = response.Split(' ');
+						System.Diagnostics.Debug.WriteLine(response);
+						if (responseArray.Length < 4)
+							continue;
+						if (!responseArray[0].Equals("650", StringComparison.OrdinalIgnoreCase) || !responseArray[1].Equals("CIRC", StringComparison.OrdinalIgnoreCase))
+							continue;
+					} while (responseArray.Length < 4 || !responseArray[3].Equals("CLOSED", StringComparison.OrdinalIgnoreCase));
+				}
+				catch (TorException)
+				{
+					throw;
 				}
 				catch (Exception ex)
 				{
