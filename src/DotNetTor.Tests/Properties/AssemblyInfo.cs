8499b780203a833d1fdf6d101adab497be4a95b2
nopara73 <adam.ficsor73@gmail.com>
2017-04-12 13:52:41 +0800

    Make tests run paralell

--- src/DotNetTor.Tests/Properties/AssemblyInfo.cs
+++ src/DotNetTor.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -8,6 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DotNetTor.Tests")]
 [assembly: AssemblyTrademark("")]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true, MaxParallelThreads = 1)]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
