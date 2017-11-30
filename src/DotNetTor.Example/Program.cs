7315d2a3511966af71d59e2f974077fe8a95ad63
nopara73 <adam.ficsor73@gmail.com>
2017-11-30 14:02:02 -0400

    make example main async

--- src/DotNetTor.Example/Program.cs
+++ src/DotNetTor.Example/Program.cs
@@ -3,73 +3,76 @@ using System.Net.Http;
 using DotNetTor.SocksPort;
 using System.Linq;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace DotNetTor.Example
 {
 	public class Program
 	{
 		// For proper configuraion see https://github.com/nopara73/DotNetTor
-		private static void Main()
+#pragma warning disable IDE1006 // Naming Styles
+		private static async Task Main()
+#pragma warning restore IDE1006 // Naming Styles
 		{
-			DoARandomRequest();
-			RequestWith3Ip();
-			//CanRequestDifferentDomainsWithSameHandler();
+			await DoARandomRequestAsync();
+			await RequestWith3IpAsync();
+			await CanRequestDifferentDomainsWithSameHandlerAsync();
 
 			Console.WriteLine("Press a key to exit..");
 			Console.ReadKey();
 		}
 
-		private static void DoARandomRequest()
+		private static async Task DoARandomRequestAsync()
 		{
 			using (var httpClient = new HttpClient(new SocksPortHandler("127.0.0.1", 9050)))
 			{
-				HttpResponseMessage message = httpClient.GetAsync("http://api.qbit.ninja/whatisit/what%20is%20my%20future").Result;
-				var content = message.Content.ReadAsStringAsync().Result;
+				HttpResponseMessage message = await httpClient.GetAsync("http://api.qbit.ninja/whatisit/what%20is%20my%20future");
+				var content = await message.Content.ReadAsStringAsync();
 				Console.WriteLine(content);
 			}
 		}
 
-		private static void RequestWith3Ip()
+		private static async Task RequestWith3IpAsync()
 		{
 			var requestUri = "https://api.ipify.org/";
 
 			// 1. Get real IP
 			using (var httpClient = new HttpClient())
 			{
-				var message = httpClient.GetAsync(requestUri).Result;
-				var content = message.Content.ReadAsStringAsync().Result;
+				var message = await httpClient.GetAsync(requestUri);
+				var content = await message.Content.ReadAsStringAsync();
 				Console.WriteLine($"Your real IP: \t\t{content}");
 			}
 
 			// 2. Get TOR IP
 			using (var httpClient = new HttpClient(new SocksPortHandler("127.0.0.1", socksPort: 9050)))
 			{
-				var message = httpClient.GetAsync(requestUri).Result;
-				var content = message.Content.ReadAsStringAsync().Result;
+				var message = await httpClient.GetAsync(requestUri);
+				var content = await message.Content.ReadAsStringAsync();
 				Console.WriteLine($"Your TOR IP: \t\t{content}");
 
 				// 3. Change TOR IP
 				var controlPortClient = new ControlPort.Client("127.0.0.1", controlPort: 9051, password: "ILoveBitcoin21");
-				controlPortClient.ChangeCircuitAsync().Wait();
+				await controlPortClient.ChangeCircuitAsync();
 
 				// 4. Get changed TOR IP
-				message = httpClient.GetAsync(requestUri).Result;
-				content = message.Content.ReadAsStringAsync().Result;
+				message = await httpClient.GetAsync(requestUri);
+				content = await message.Content.ReadAsStringAsync();
 				Console.WriteLine($"Your other TOR IP: \t{content}");
 			}
 		}
-		private static void CanRequestDifferentDomainsWithSameHandler()
+		private static async Task CanRequestDifferentDomainsWithSameHandlerAsync()
 		{
 			using (var httpClient = new HttpClient(new SocksPortHandler()))
 			{
-				var message = httpClient.GetAsync("https://api.ipify.org/").Result;
-				var content = message.Content.ReadAsStringAsync().Result;
+				var message = await httpClient.GetAsync("https://api.ipify.org/");
+				var content = await message.Content.ReadAsStringAsync();
 				Console.WriteLine($"Your TOR IP: \t\t{content}");
 
 				try
 				{
-					message = httpClient.GetAsync("http://api.qbit.ninja/whatisit/what%20is%20my%20future").Result;
-					content = message.Content.ReadAsStringAsync().Result;
+					message = await httpClient.GetAsync("http://api.qbit.ninja/whatisit/what%20is%20my%20future");
+					content = await message.Content.ReadAsStringAsync();
 					Console.WriteLine(content);
 				}
 				catch (AggregateException ex) when (ex.InnerException is TorException)
