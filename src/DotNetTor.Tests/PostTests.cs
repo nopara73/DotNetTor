230def559d15db7f3bf92d76b7c461f9f0418f54
nopara73 <adam.ficsor73@gmail.com>
2018-02-12 13:31:47 +0100

    Remove leftover loginfo

--- src/DotNetTor.Tests/PostTests.cs
+++ src/DotNetTor.Tests/PostTests.cs
@@ -108,8 +108,7 @@ namespace DotNetTor.Tests
 
 				HttpResponseMessage message = await _client.PostAsync("https://api.smartbit.com.au/v1/blockchain/decodetx", content);
 				var responseContentString = await message.Content.ReadAsStringAsync();
-
-				Logger.LogInfo(responseContentString);
+				
 				Assert.Equal("{\"success\":true,\"transaction\":{\"Version\":\"1\",\"LockTime\":\"0\",\"Vin\":[{\"TxId\":null,\"Vout\":null,\"ScriptSig\":null,\"CoinBase\":\"03a58605204d696e656420627920416e74506f6f6c20757361311f10b53620558903d80272a70c0000724c0600\",\"TxInWitness\":null,\"Sequence\":\"4294967295\"}],\"Vout\":[{\"Value\":25.21865743,\"N\":0,\"ScriptPubKey\":{\"Asm\":\"OP_DUP OP_HASH160 2ef12bd2ac1416406d0e132e5bc8d0b02df3861b OP_EQUALVERIFY OP_CHECKSIG\",\"Hex\":\"76a9142ef12bd2ac1416406d0e132e5bc8d0b02df3861b88ac\",\"ReqSigs\":1,\"Type\":\"pubkeyhash\",\"Addresses\":[\"15HCzh8AoKRnTWMtmgAsT9TKUPrQ6oh9HQ\"]}}],\"TxId\":\"a02b9bd4264ab5d7c43ee18695141452b23b230b2a8431b28bbe446bf2b2f595\"}}", responseContentString);
 			}
 		}
